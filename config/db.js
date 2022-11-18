4f602d2fa12560feda70ff1227ab21d8ed094e53
JohnMejia <john.mejias@udea.edu.co>
2022-11-18 12:52:29 -0500

    se quita secure layer

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const pool = mysql({
     user: "w0bsjgt0fp4crabi9347",
     password: "pscale_pw_byt8n3medBjmT0yEBq3Bs8InLGgcIss1L90HOT48F8n",
     database: "learningdb",
-    ssl:{"rejectUnauthorized":true}
+    ssl:{"rejectUnauthorized":false}
   },
 });
 
