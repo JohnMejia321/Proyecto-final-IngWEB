82facc8f5edd58f4bf5fc6fd2b76cda020dfa175
JohnMejia <john.mejias@udea.edu.co>
2022-11-17 20:27:52 -0500

    se sube la base de datos mysql a un hosting en la nube

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ import mysql from "serverless-mysql";
 
 const pool = mysql({
   config: {
-    host: "localhost",
-    user: "root",
-    password: "fredy555",
-    port: 3306,
-    database: "productsdb",
+    host: "us-east.connect.psdb.cloud",
+    user: "w0bsjgt0fp4crabi9347",
+    password: "pscale_pw_byt8n3medBjmT0yEBq3Bs8InLGgcIss1L90HOT48F8n",
+    database: "learningdb",
+    ssl:{"rejectUnauthorized":true}
   },
 });
 
